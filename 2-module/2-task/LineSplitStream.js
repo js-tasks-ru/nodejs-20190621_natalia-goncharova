b8c5ada1b830e76ea7732e7255336605f90de5b2
Natalia <gon4arovanatalija@gmail.com>
2019-07-02 13:43:52 +0300

    +

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -4,12 +4,31 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.lastLineData ='';
   }
 
   _transform(chunk, encoding, callback) {
+    const data = this.lastLineData + chunk.toString();
+    const lines = data.split(os.EOL);
+    const lastLine = lines.pop();
+    this.lastLineData ='';
+    for (const line of lines) {
+      this.push(line);
+    }
+    if (lastLine.endsWith(os.EOL)) {
+      this.push(lastLine);
+    } else {
+      this.lastLineData = lastLine;
+    }
+    callback();
   }
 
   _flush(callback) {
+    if (this.lastLineData) {
+      this.push(this.lastLineData);
+    }
+    callback();
   }
 }
 
